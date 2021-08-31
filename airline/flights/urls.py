0397cd52cab43548d047a932f5c060ccf9231e68
DAVID E. BARRERA PARRA <david.barrera@iroute.com.ec>
2021-08-31 14:51:00 -0500

    Add Booking feature

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('details/<int:flight_id>', views.flight, name='details'),
+    path('book/<int:flight_id>', views.book, name='book'),
 ]
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
-from .models import Airport, Flight
+from .models import Airport, Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -12,3 +14,12 @@ def flight(request, flight_id):
     context['flight'] = Flight.objects.get(id=flight_id)
     context['passengers'] = context['flight'].passengers.all()
     return render(request, 'flights/details.html', context)
+
+def book(request, flight_id):
+    if request.method == 'POST':
+        flight = Flight.objects.get(pk=flight_id)
+        # TODO: check for duplicates
+        passenger = Passenger.objects.create(first=request.POST['passenger_first'], last=request.POST['passenger_last'])
+        passenger.save()
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse('details', args=(flight.id,)))
