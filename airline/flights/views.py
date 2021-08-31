d0fb99e17c5a6f5e6b424818dfb7d66b003287e4
DAVID E. BARRERA PARRA <david.barrera@iroute.com.ec>
2021-08-31 13:33:49 -0500

    Show Flights

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Airport, Flight
 
 # Create your views here.
 def index(request):
     context = {}
+    context['flights'] = Flight.objects.all()
     return render(request, 'flights/index.html', context)
